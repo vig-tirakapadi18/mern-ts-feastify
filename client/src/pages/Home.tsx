f998d6f1dec9f31930593072d50cb57fde14e050
Vighnesh Tirakapadi <vighnesh.tirakapadi@deeporion.com>
2025-01-01 11:17:13 +0530

    added mobile app suggestion

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,6 @@
 import React, { FC } from "react";
+import mobileResponsive from "../assets/mobile.png";
+import appDownload from "../assets/appDownload.png";
 
 const Home: FC = (): React.JSX.Element => {
   return (
@@ -9,6 +11,21 @@ const Home: FC = (): React.JSX.Element => {
         </h1>
 
         <span className="text-xl">Food is just a click away!</span>
+      </div>
+
+      <div className="grid md:grid-cols-2 gap-5">
+        <img src={mobileResponsive} alt="Phone Screen" />
+        <div className="flex flex-col items-center justify-center gap-4 text-center">
+          <span className="font-bold text-xl tracking-tighter">
+            Order takeway even faster!
+          </span>
+          <span>
+            Download the <b>Feastify!</b> app for faster ordering and
+            personalized recommendations.
+          </span>
+
+          <img src={appDownload} alt="playstore appstore" />
+        </div>
       </div>
     </div>
   );
