efa9a428acc3a5cf64a17b679712c0e0222e0683
Vighnesh Tirakapadi <vighnesh.tirakapadi@deeporion.com>
2025-01-01 16:09:59 +0530

    added Auth0 for authentication

--- client/src/components/MainNav.tsx
+++ client/src/components/MainNav.tsx
@@ -1,11 +1,15 @@
 import React, { FC } from "react";
 import { Button } from "./ui/button";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const MainNav: FC = (): React.JSX.Element => {
+  const { loginWithRedirect } = useAuth0();
+
   return (
     <Button
       variant="ghost"
       className="font-bold bg-emerald-500 text-white hover:bg-white hover:text-emerald-500 hover:border-emerald-500 border-[1px] outline-none text-lg px-10 py-5 rounded-full"
+      onClick={async () => await loginWithRedirect()}
     >
       Sign In
     </Button>
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate.tsx'
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Auth0ProviderWithNavigate>
+        <App />
+      </Auth0ProviderWithNavigate>
     </BrowserRouter>
   </StrictMode>
 );
