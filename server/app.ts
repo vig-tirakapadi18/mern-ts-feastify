65ff480b535abd947b926f2570bebbc5f697d0e9
Vighnesh Tirakapadi <vighnesh.tirakapadi@deeporion.com>
2025-01-04 18:47:11 +0530

    connected to cloudinary

--- server/app.ts
+++ server/app.ts
@@ -5,10 +5,14 @@ import { connectToDB } from "./src/db/connectToDB";
 
 // Routes
 import userRoutes from "./src/routes/user.route";
+import { connectToCloudinary } from "./src/db/connectToCloudinary";
 
 const app = express();
 
+// Connect to MongoDB and Cloudinary databases before starting the server
 connectToDB();
+connectToCloudinary();
+
 app.use(express.json());
 app.use(cors());
 
