efbd15b4906fcb497c150cc249e61c6c3e2d063b
Vighnesh Tirakapadi <vighnesh.tirakapadi@deeporion.com>
2025-01-14 11:37:47 +0530

    completed Order Status page

--- client/src/components/OrderStatusHeader.tsx
+++ client/src/components/OrderStatusHeader.tsx
@@ -1,12 +1,15 @@
 import React, { FC } from "react";
 import { IOrder } from "../types";
 import { Progress } from "./ui/progress";
+import { ORDER_STATUS } from "../config/orderStatus";
 
 interface IOrderStatusHeaderProps {
   order: IOrder;
 }
 
-const OrderStatusHeader: FC<IOrderStatusHeaderProps> = ({ order }) => {
+const OrderStatusHeader: FC<IOrderStatusHeaderProps> = ({
+  order,
+}): React.JSX.Element => {
   const getExpectedDeliveryTime = () => {
     const created = new Date(order.createdAt);
 
@@ -23,13 +26,32 @@ const OrderStatusHeader: FC<IOrderStatusHeaderProps> = ({ order }) => {
     return `${paddedHours}:${paddedMins}`;
   };
 
+  const getOrderStatusInfo = () => {
+    return (
+      ORDER_STATUS.find((o) => o.value === order.status) || ORDER_STATUS[0]
+    );
+  };
+
   return (
     <>
-      <h1 className="text-4xl font-bold tracking-tighter flex flex-col gap-5 md:flex-row md:justify-between">
-        <span>Order Status: {order.status}</span>
-        <span>Expected by: {getExpectedDeliveryTime()}</span>
+      <h1 className="text-2xl font-bold tracking-tighter flex flex-col md:flex-row md:justify-between mb-2">
+        <span className="font-light">
+          Order Status:{" "}
+          <span className="font-semibold text-emerald-500">
+            {getOrderStatusInfo().label}
+          </span>
+        </span>
+        <span className="font-light">
+          Expected by:{" "}
+          <span className="font-semibold text-emerald-500">
+            {getExpectedDeliveryTime()}
+          </span>
+        </span>
       </h1>
-      <Progress className="animate-pulse" />
+      <Progress
+        className="animate-pulse bg-emerald-300"
+        value={getOrderStatusInfo().progressValue}
+      />
     </>
   );
 };
--- client/src/pages/OrderStatus.tsx
+++ client/src/pages/OrderStatus.tsx
@@ -2,6 +2,8 @@ import React, { FC } from "react";
 import { useGetAllOrders } from "../api/OrderApi";
 import LoadingSpinner from "../components/LoadingSpinner";
 import OrderStatusHeader from "../components/OrderStatusHeader";
+import OrderStatusDetail from "../components/OrderStatusDetail";
+import { AspectRatio } from "../components/ui/aspect-ratio";
 
 const OrderStatus: FC = (): React.JSX.Element => {
   const { orderResponse, isLoading } = useGetAllOrders();
@@ -16,9 +18,20 @@ const OrderStatus: FC = (): React.JSX.Element => {
       {orderResponse.orders.map((order) => (
         <div
           key={crypto.randomUUID()}
-          className="space-y-10 bg-gray-50 p-10 rounded-lg"
+          className="space-y-10 bg-gray-50 px-10 py-10 rounded-lg shadow-lg border-emerald-200 border-[0.5px]"
         >
           <OrderStatusHeader order={order} />
+
+          <div className="grid gap-10 md:grid-cols-2">
+            <OrderStatusDetail order={order} />
+            <AspectRatio ratio={16 / 5}>
+              <img
+                src={order.restaurant.imgUrl}
+                alt={order.restaurant.restaurantName}
+                className="rounded-md object-cover h-full w-full"
+              />
+            </AspectRatio>
+          </div>
         </div>
       ))}
     </section>
