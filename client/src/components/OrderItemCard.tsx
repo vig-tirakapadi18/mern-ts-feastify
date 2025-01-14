8c832a1099ba61ddb7bc4c6fee6a1b53249d6cea
Vighnesh Tirakapadi <vighnesh.tirakapadi@deeporion.com>
2025-01-14 13:31:24 +0530

    added orders page for restaurants for managing orders

---
+++ client/src/components/OrderItemCard.tsx
@@ -0,0 +1,100 @@
+import React, { FC } from "react";
+import { IOrder } from "../types";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { Separator } from "./ui/separator";
+import { Badge } from "./ui/badge";
+import { Label } from "./ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { ORDER_STATUS } from "../config/orderStatus";
+
+interface IOrderItemCardProps {
+  order: IOrder;
+}
+
+const OrderItemCard: FC<IOrderItemCardProps> = ({ order }) => {
+  const getTime = () => {
+    const orderDateTime = new Date(order.createdAt);
+
+    const hours = orderDateTime.getHours();
+    const mins = orderDateTime.getMinutes();
+
+    const paddedHours = hours < 10 ? `0${hours}` : hours;
+    const paddedMins = mins < 10 ? `0${mins}` : mins;
+
+    return `${paddedHours}:${paddedMins}`;
+  };
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="grid md:grid-cols-4 gap-4 justify-between mb-3">
+          <div className="">
+            <span>Customer Name</span>
+            <span className="ml-2 font-normal">
+              {order.deliveryDetails.name}
+            </span>
+          </div>
+
+          <div className="">
+            <span>Delivery Address:</span>
+            <span className="ml-2 font-normal">
+              {order.deliveryDetails.addressLine1}, {order.deliveryDetails.city}
+            </span>
+          </div>
+
+          <div className="">
+            <span>Time:</span>
+            <span className="ml-2 font-normal">{getTime()}</span>
+          </div>
+
+          <div className="">
+            <span>Total Cost:</span>
+            <span className="ml-2 font-normal">
+              Rs {(order.totalAmount / 100).toFixed(2)}
+            </span>
+          </div>
+        </CardTitle>
+
+        <Separator />
+      </CardHeader>
+
+      <CardContent className="flex flex-col gap-6">
+        <div className="flex flex-col gap-2">
+          {order.cartItems.map((item) => (
+            <span key={crypto.randomUUID()}>
+              <Badge variant="outline" className="mr-2">
+                {item.quantity}
+              </Badge>
+              {item.name}
+            </span>
+          ))}
+        </div>
+
+        <div className="flex flex-col space-y-1.5">
+          <Label htmlFor="status">What is the status of this order?</Label>
+
+          <Select>
+            <SelectTrigger id="status">
+              <SelectValue placeholder="Status" />
+
+              <SelectContent position="popper">
+                {ORDER_STATUS.map((status) => (
+                  <SelectItem key={crypto.randomUUID()} value={status.value}>
+                    {status.label}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </SelectTrigger>
+          </Select>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default OrderItemCard;
